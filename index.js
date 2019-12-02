5babebf18a1702bb8b65ecf1d9e31e1c3c28e1a9
unbyt <58178494+unbyt@users.noreply.github.com>
2019-12-02 08:39:44 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
+      const ytdl = require('ytdl-core');
+      const fs = require('fs');
 
+      function getFile() {
+        try {
+          var text = document.getElementById("videoinput").value;
+          ytdl(text).pipe(fs.createWriteStream('video.mp4'));
+          alert("Completed!");
+        } catch(e) {
+          alert(e);
+        } 
+      }
